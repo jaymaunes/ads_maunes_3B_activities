6ead0109a508db1b985398d983a55ac626f415d8
jay maunes <jaymaunes4@gmail.com>
2024-08-29 17:55:24 +0800

    Completed Activity 2: Database Configuration and Migration Basics,

---
+++ Activity2.py
@@ -0,0 +1,16 @@
+
+age = int(23)
+print("Age: ", age)
+
+
+height = float(5.5)
+print("Height: ", height)
+
+
+name = str("Dexter T. Hermogino")
+print("Name: ", name)
+
+
+student = True
+print("Student: ", student)
+
